533a0211cd659e431a6ec5fedf6c4dcb6728ddec
Don Jose <donnjondanmakal@gmail.com>
2022-12-01 18:12:55 +0530

    Raffle.sol

---
+++ contracts/Raffle.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+error Raffle_NotEnoughETHAmount();
+
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughETHAmount();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
+//14:10
